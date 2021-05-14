c4d16779d31379f0fdf790805da184be6c20e6aa
ramesh lal das <lalrameshdas@gmail.com>
2021-05-14 23:32:03 +0530

    balaram

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Student from "./Student";
+const App = () =>{
+    return (
+        <>
+        <Student name="Durga Sir" />
+        <Student name="Ramesh"  rollno="Roll-101"/>
+        <Student name="Durga Sir" addrs="Ammirpet" />
+        </>
+
+    )
+}
+export default App;
---
+++ src/Student.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const Student = (props) =>{
+    return(
+        <h1> Hello {props.name} {props.rollno} {props.addrs}</h1>
+    )
+}
+export default Student;
